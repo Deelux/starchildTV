18c1c6932608f860ed96ef30a8e2c437eddc31fd
Dominic Quentin <dominicquentin@Dominics-MacBook-Pro.local>
2021-01-22 13:20:18 +0100

    Added starter files

---
+++ gatsby/src/pages/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Layout from '../components/Layout';
+
+export default function HomePage() {
+  return (
+    <Layout>
+      <p>I'm the home page!</p>
+    </Layout>
+  );
+}
---
+++ gatsby/src/pages/slicemasters.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Layout from '../components/Layout';
+
+export default function SlicemastersPage() {
+  return (
+    <Layout>
+      <p>I'm the slicemaster page!</p>
+    </Layout>
+  );
+}
