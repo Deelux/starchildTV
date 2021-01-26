075a06ac7dad0edc77ae961f1383378395ce825d
Dominic Quentin <dominic.quentin@gmail.com>
2021-01-26 11:38:30 +0100

    Added SiteMetadata

--- gatsby/.cache/api-runner-ssr.js
+++ gatsby/.cache/api-runner-ssr.js
@@ -1,4 +1,7 @@
-var plugins = []
+var plugins = [{
+      plugin: require('/Users/dominicquentin/workspace/master-gatsby/gatsby/gatsby-ssr'),
+      options: {"plugins":[]},
+    }]
 // During bootstrap, we write requires at top of this file which looks like:
 // var plugins = [
 //   {
