ea8928f76b96e788b641cc1bf89b4417dd7c9dfa
Dominic Quentin <dominic.quentin@gmail.com>
2021-02-03 07:58:00 +0100

    Scaffolding and styling slicemasters page

--- gatsby/src/pages/slicemasters.js
+++ gatsby/src/pages/slicemasters.js
@@ -1,9 +1,83 @@
+import { graphql, Link } from 'gatsby';
 import React from 'react';
+import Img from 'gatsby-image';
+import styled from 'styled-components';
 
-export default function SlicemastersPage() {
+const SlicemasterGrid = styled.div`
+  display: grid;
+  gap: 2rem;
+  grid-template-columns: repeat(auto-fit, minmax(250px, 1fr));
+`;
+
+const StyledSlicemasters = styled.div`
+  a {
+    text-decoration: none;
+  }
+  .gatsby-image-wrapper {
+    height: 400px;
+  }
+  h2 {
+    transform: rotate(-2deg);
+    text-align: center;
+    font-style: 4rem;
+    margin-bottom: -2rem;
+    position: relative;
+    z-index: 2;
+  }
+  .description {
+    background-color: var(--yellow);
+    text-align: center;
+    padding: 1rem;
+    margin: 2rem;
+    margin-top: -6rem;
+    z-index: 2;
+    position: relative;
+    transform: rotate(1deg);
+  }
+`;
+
+export default function SlicemastersPage({ data }) {
+  const persons = data.persons.nodes;
+  console.log(persons);
   return (
     <>
-      <p>I'm the slicemaster page!</p>
+      <p>I'm the slicemaster page! There are {persons.length} inputs.</p>
+      <SlicemasterGrid>
+        {persons.map((person) => (
+          <StyledSlicemasters key={person.id}>
+            <Link to={`/slicemaster/${person.slug.current}`}>
+              <h2>
+                <span className="mark">{person.name}</span>
+              </h2>
+            </Link>
+            <Img fluid={person.image.asset.fluid} alt={person.name} />
+            <p className="description">{person.description}</p>
+          </StyledSlicemasters>
+        ))}
+      </SlicemasterGrid>
     </>
   );
 }
+
+export const query = graphql`
+  query PersonQuery {
+    persons: allSanityPerson {
+      totalCount
+      nodes {
+        id
+        name
+        description
+        slug {
+          current
+        }
+        image {
+          asset {
+            fluid(maxWidth: 400) {
+              ...GatsbySanityImageFluid
+            }
+          }
+        }
+      }
+    }
+  }
+`;
