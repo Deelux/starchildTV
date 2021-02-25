f52ccd15f85a8cfd576383ca4059cacf959ee482
Dominic Quentin <dominic.quentin@gmail.com>
2021-02-25 20:47:14 +0100

    Added teaser

--- gatsby/src/pages/index.js
+++ gatsby/src/pages/index.js
@@ -5,6 +5,7 @@ export default function HomePage() {
     <div className="center">
       <h1>StarChildTV</h1>
       <p>On a Mision to Help 1 Billion People Navigate the Digital World.</p>
+      <p>More information coming soon...</p>
     </div>
   );
 }
